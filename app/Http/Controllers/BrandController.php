8dbd865908a7548ce9a0fdb844ddccd26a300d5b
EduardoJCentenoL <centenolopezeduaardojose@gmail.com>
2026-06-14 00:04:58 -0600

    feat(BrandController): added the methods of the controller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,21 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandRequest;
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BrandController extends Controller
 {
-    public function index() {}
+    //? Mostrar el listado de Marcas
+    public function index() {
+        $brands = Brand::all();
+        return view('brands.index', compact('brands'));
+    }
 
-    public function create(){}
+    //? Mostrar el formulario para crear una nueva Marca
+    public function create(){
+        $brand = new Brand();
+        return view('brands.create', compact('brand'));
+    }
 
-    public function store(Request $request, string $id){}
+    //? Guarda una Marca recien creada en la base de datos
+    public function store(BrandRequest $request){
+        Brand::create($request->validated());
+        return redirect()->route('brands.index')->with('succes','Marca Creada');
+    }
 
-    public function show(string $id){}
+    //? Muestra una Marca especifica (opcional, pero mejor que este)
+    public function show(string $id){
+        $brand = Brand::findOrFail($id);
+        return view('Brand.show', compact('brand'));
+    }
 
-    public function edit(string $id){}
+    //? Muestra el formulario para editar una Marca existente
+    public function edit(string $id){
+        $brand = Brand::findOrFail($id);
+        return view('brands.edit', compact('brand'));
+    }
 
-    public function update(Request $request, string $id){}
+    //? Actualiza una Marca especifica en la base de datos
+    public function update(BrandRequest $request, Brand $brand){
+        // $brand = Brand::update($request->validated());
+        $brand->update($request->validated());
 
-    public function destroy(string $id){}
+        return redirect()->route('brands.index')->with('succes', 'Marca Actualizada');
+    }
+
+    public function destroy(string $id){
+        $brand = Brand::findOrFail($id);
+        $brand->delete();
+
+        return redirect()->route('brands.index')->with('succes', 'Marca Eliminada');
+    }
 }
